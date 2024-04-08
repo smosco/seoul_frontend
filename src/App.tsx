3e7ce71773b3c77655c1e02e0cf669b135a0ddd8
smosco <go1ruf2tk3@gmail.com>
2024-04-08 20:32:33 +0900

    feat: kakao map api 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any;
+  }
+}
+
+const { kakao } = window;
 
 function App() {
-  return <div className="App">App</div>;
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const option = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+    const map = new kakao.maps.Map(container, option);
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="map" style={{ width: '500px', height: '500px' }} />
+    </div>
+  );
 }
 
 export default App;
