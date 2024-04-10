5c55886dbdf3ce176d8772d851b546f8ef965305
smosco <go1ruf2tk3@gmail.com>
2024-04-10 14:48:39 +0900

    feat: 현 위치 좌표를 도로명 주소로 변환

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import cctv from '../assets/images/cctv.png';
 import emergencyBell from '../assets/images/emergencybell.png';
 import safetFacility from '../assets/images/safetyfacility.png';
@@ -12,6 +12,11 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     kakao: any;
   }
+}
+
+interface AddressResult {
+  address: { address_name: string };
+  code: string;
 }
 
 const { kakao } = window;
@@ -36,42 +41,72 @@ const getImageSrc = (title: string) => {
 };
 
 function Home() {
-  const positions = [
-    {
-      title: 'cctv',
-      latlng: new kakao.maps.LatLng(33.450705, 126.570677),
-    },
-    {
-      title: 'safetyFacility',
-      latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-    },
-    {
-      title: 'fireStation',
-      latlng: new kakao.maps.LatLng(33.450879, 126.56994),
-    },
-    {
-      title: 'heatShelter',
-      latlng: new kakao.maps.LatLng(33.450705, 126.572323),
-    },
-    {
-      title: 'saftyCenter',
-      latlng: new kakao.maps.LatLng(33.451502, 126.570766),
-    },
-    {
-      title: 'emergencyBell',
-      latlng: new kakao.maps.LatLng(33.449802, 126.570722),
-    },
-  ];
+  const [currentPosition, setCurrentPosition] =
+    useState<GeolocationPosition | null>(null);
 
   useEffect(() => {
+    // 위치 정보 요청
+    navigator.geolocation.getCurrentPosition(
+      (position: GeolocationPosition) => {
+        setCurrentPosition(position);
+      },
+      (error: GeolocationPositionError) => {
+        console.error('Error getting current position:', error);
+      },
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!currentPosition) return;
+
     const container = document.getElementById('map');
     const option = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      center: new kakao.maps.LatLng(
+        currentPosition.coords.latitude,
+        currentPosition.coords.longitude,
+      ),
       level: 3,
     };
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
     const map = new kakao.maps.Map(container, option);
+
+    const positions = [
+      {
+        title: 'cctv',
+        latlng: new kakao.maps.LatLng(37.486592, 126.9409552),
+      },
+      {
+        title: 'safetyFacility',
+        latlng: new kakao.maps.LatLng(37.486592, 126.938552),
+      },
+      {
+        title: 'fireStation',
+        latlng: new kakao.maps.LatLng(37.485592, 126.9409552),
+      },
+      {
+        title: 'heatShelter',
+        latlng: new kakao.maps.LatLng(37.487592, 126.9399552),
+      },
+      {
+        title: 'saftyCenter',
+        latlng: new kakao.maps.LatLng(37.486592, 126.9389552),
+      },
+      {
+        title: 'emergencyBell',
+        latlng: new kakao.maps.LatLng(37.486592, 126.9389552),
+      },
+    ];
+
+    // 현재 위치 마커 생성 및 추가
+    const currentPositionMarker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(
+        currentPosition.coords.latitude,
+        currentPosition.coords.longitude,
+      ),
+      image: new kakao.maps.MarkerImage(location, new kakao.maps.Size(24, 32)),
+    });
+    currentPositionMarker.setMap(map);
 
     for (let i = 0; i < positions.length; i++) {
       const imageSrc = getImageSrc(positions[i].title);
@@ -79,15 +114,33 @@ function Home() {
 
       const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-      // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
       const marker = new kakao.maps.Marker({
         map,
         position: positions[i].latlng,
         title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         image: markerImage,
       });
+      marker.setMap(map);
     }
-  }, []);
+  }, [currentPosition]);
+
+  useEffect(() => {
+    if (!currentPosition) return;
+
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    const coord = new kakao.maps.LatLng(
+      currentPosition?.coords.latitude,
+      currentPosition?.coords.longitude,
+    );
+    const callback = (result: AddressResult[], status: string) => {
+      if (status === kakao.maps.services.Status.OK) {
+        console.log(result, result[0].address.address_name);
+      }
+    };
+
+    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+  }, [currentPosition]);
 
   return <div id="map" style={{ width: '500px', height: '500px' }} />;
 }
