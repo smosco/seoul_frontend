f00fb220c1305122c16d31cde92e5baad96fc6aa
smosco <go1ruf2tk3@gmail.com>
2024-04-12 11:14:38 +0900

    feat: SearchState 공통 타입으로 이동

--- src/types/mapTypes.ts
+++ src/types/mapTypes.ts
@@ -1,1 +1,13 @@
-export type FacilitiesType = 'cctv' | 'fireStation' | 'safetyFacility' | 'saftyCenter' | 'emergencyBell' | 'heatShelter';
+export type FacilitiesType =
+  | 'cctv'
+  | 'fireStation'
+  | 'safetyFacility'
+  | 'saftyCenter'
+  | 'emergencyBell'
+  | 'heatShelter';
+
+export interface SearchState {
+  keyword: string;
+  isSearching: boolean;
+  selectedName: string;
+}
