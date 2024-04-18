9ddb3f56600b7caee325c24db275cac825005bab
smosco <go1ruf2tk3@gmail.com>
2024-04-18 11:19:03 +0900

    feat: 수정된 경유지 데이터로 변경

--- src/pages/RouteExplorer.tsx
+++ src/pages/RouteExplorer.tsx
@@ -34,9 +34,39 @@ function RouteExplorer() {
   }>();
 
   const [waypoints] = useState([
-    { latitude: 37.48211807, longitude: 126.94151563 },
-    { latitude: 37.48334492, longitude: 126.94311689 },
-    { latitude: 37.48161125, longitude: 126.94127377 },
+    {
+      id: 16684.0,
+      longitude: 127.013,
+      latitude: 37.4932,
+      rank_score: 56.84,
+      emergency_bell_and_distance_score: 11.89,
+      safety_center_and_distacne_score: 19.85,
+      grid_shelter_distance_score: 77.78,
+      grid_facilities_distance_score: 55.56,
+      number_of_cctv_score: 5.48,
+    },
+    {
+      id: 17009.0,
+      longitude: 127.018,
+      latitude: 37.4554,
+      rank_score: 98.97,
+      emergency_bell_and_distance_score: 49.88,
+      safety_center_and_distacne_score: 52.04,
+      grid_shelter_distance_score: 100.0,
+      grid_facilities_distance_score: 100.0,
+      number_of_cctv_score: 24.94,
+    },
+    {
+      id: 17161.0,
+      longitude: 127.02,
+      latitude: 37.4554,
+      rank_score: 98.76,
+      emergency_bell_and_distance_score: 49.76,
+      safety_center_and_distacne_score: 50.22,
+      grid_shelter_distance_score: 100.0,
+      grid_facilities_distance_score: 100.0,
+      number_of_cctv_score: 100.0,
+    },
   ]);
 
   useEffect(() => {
