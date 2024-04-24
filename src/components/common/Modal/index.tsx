25372f5ba0fba50ee9523eef2dc5d9c25afbffbd
stu442 <stu442@naver.com>
2024-04-24 17:50:50 +0900

    feat : 공용 Modal 컴포넌트

---
+++ src/components/common/Modal/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState , createContext, useContext } from 'react';
+import { InnerWrapper, OuterWrapper } from './style';
+
+interface ModalContextProps {
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ModalContext = createContext<ModalContextProps | null>(null);
+
+const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error('useModal은 반드시 ModalProvider 안에 있어야합니다.');
+  }
+  return context;
+};
+
+function Modal({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contextValue = React.useMemo(() => ({ isOpen, setIsOpen }), [isOpen, setIsOpen]);
+
+  return (
+    <ModalContext.Provider value={contextValue}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Toggle({ children }: { children: React.ReactNode }) {
+  const { setIsOpen } = useModal();
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div onClick={() => setIsOpen(prev => !prev)}>
+      {children}
+    </div>
+  );
+}
+
+function Content({ children }: { children: React.ReactNode }) {
+  const { isOpen, setIsOpen } = useModal();
+  return isOpen ?
+    <>
+      <OuterWrapper onMouseDown={() => setIsOpen(false)} />
+      <InnerWrapper>{children}</InnerWrapper>
+    </>
+    : null;
+}
+
+Modal.Toggle = Toggle;
+Modal.Content = Content;
+
+export default Modal;
