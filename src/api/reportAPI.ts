bdb0e0b979ce35ebb20e960dccde594e79288611
stu442 <stu442@naver.com>
2024-04-30 14:56:02 +0900

    fix : API https 변경사항 수정 및 API 분리

--- src/api/reportAPI.ts
+++ src/api/reportAPI.ts
@@ -3,7 +3,7 @@ import { ReportData } from '../types/reportTypes';
 
 export const getReportData = async () => {
   try {
-    const response = await axios.get('http://3.34.25.245:80/api/report');
+    const response = await axios.get('https://wemate.site/api/report');
     return response.data;
   } catch (error) {
     throw new Error('신고 데이터를 불러오는데 실패했습니다.');
@@ -12,7 +12,7 @@ export const getReportData = async () => {
 
 export const postReportData = async (data: ReportData) => {
   try {
-    const response = await axios.post('http://3.34.25.245:80/api/report', data);
+    const response = await axios.post('https://wemate.site/api/report', data);
     return response;
   } catch (error) {
     throw new Error('네트워크 오류로 인해 신고하는데 실패했습니다. 다시 시도해주세요.');
