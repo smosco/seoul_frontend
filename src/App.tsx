6993c79bf8ba919ca63502281bbf66e5a74ea14e
stu442 <stu442@naver.com>
2024-05-04 14:38:03 +0900

    design : NotoSans 폰트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import EmergencyPage from './pages/Report';
 import RouteExplorer from './pages/RouteExplorer';
 import DetailRoute from './pages/DetailRoute';
+import GlobalStyle from './styles/GlobalStyles';
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
+        <GlobalStyle />
         <RouterProvider router={router} />
       </RecoilRoot>
     </QueryClientProvider>
